4082a2befb455ededae5676e65dc193ad77b34cc
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-04-11 14:42:51 -0300

    Actualización Clase 3

---
+++ Clase_3/Ejemplo1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Teoría
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad;
+
+            Console.WriteLine("Cantidad de apellidos en la lista:");
+
+            cantidad = int.Parse(Console.ReadLine());
+
+            string[] listaApellidos = new string[cantidad];
+
+            AgregarApellidos(listaApellidos);
+
+            Console.WriteLine("\nApellidos de la lista:");
+
+            ImprimirApellidos(listaApellidos);
+        }
+
+        public static void ImprimirApellidos(string[] apellidos)
+        {
+            foreach (string apellido in apellidos)
+            {
+                Console.WriteLine(apellido);
+            }
+        }
+        public static void AgregarApellidos(string[] apellidos)
+        {
+            for (int i = 0; i < apellidos.Length; i++)
+            {
+                Console.WriteLine("Apellido:");
+
+                AgregarApellidoALaLista(Console.ReadLine(), i, ref apellidos);
+            }
+        }
+        public static void AgregarApellidoALaLista(string apellido, int indice, ref string[] apellidos)
+        {
+            apellidos[indice] = apellido;
+        }
+    }    
+}
---
+++ Clase_3/Ejercicio_6_Revisión/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ejercicio_6_Revisión
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            int[] cantidadVocales = new int[5] { 0, 0, 0, 0, 0 };
+
+            char[] vocales = new char[5] { 'a','e','i','o','u'};
+
+            Console.WriteLine("Ingrese una palabra:");
+
+            palabra = Console.ReadLine();
+
+            contarVocales(palabra, ref cantidadVocales);
+
+            for (int i = 0; i < cantidadVocales.Length; i++)
+            {
+                Console.WriteLine($"Cantidad de letras {vocales[i]}: {cantidadVocales[i]}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void contarVocales(string palabra, ref int[] cantidadVocales)
+        {
+            foreach (char letra in palabra)
+            {
+                switch (letra)
+                {
+                    case 'a':
+                        cantidadVocales[0]++;
+                        break;
+                    case 'e':
+                        cantidadVocales[1]++;
+                        break;
+                    case'i':
+                        cantidadVocales[2]++;
+                        break;
+                    case 'o':
+                        cantidadVocales[3]++;
+                        break;
+                    case 'u':
+                        cantidadVocales[4]++;
+                        break;
+                }
+            }
+        }
+    }
+}
