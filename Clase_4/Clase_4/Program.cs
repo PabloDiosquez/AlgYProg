0fa0dc310f0c6edc686b27a205ae8b7c47fc4498
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-04-26 13:51:26 -0300

    Actualización Clase 4

--- Clase_4/Clase_4/Program.cs
+++ Clase_4/Clase_4/Program.cs
@@ -6,7 +6,17 @@ namespace Clase_4
     {
         static void Main(string[] args)
         {
-             
+            MiPrimerClase objeto1 = new MiPrimerClase();
+
+            objeto1.Imprimir();
+        }
+
+        public class MiPrimerClase
+        {
+            public void Imprimir()
+            {
+                Console.WriteLine("¡Perfecto, funcionó!");
+            }
         }
     }
 }
