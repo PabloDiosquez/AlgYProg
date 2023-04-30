d7dff7c49f69293f4e2ac578ddd0ec4dcd971c94
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-04-30 16:57:03 -0300

    Actualización Clase 5

---
+++ Clase_5/Consola/Veterinaria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Veterinaria
+    {
+        // Atributos
+
+        string nombre;
+
+        private Mascota[] listaDeMascotas;
+
+        private int cantidadDeMascotas;
+
+        // Constructor
+
+        public Veterinaria(string nombre)
+        {
+            this.nombre = nombre;   
+
+            listaDeMascotas = new Mascota[10];
+
+            cantidadDeMascotas = 0;
+        }
+    }
+}
