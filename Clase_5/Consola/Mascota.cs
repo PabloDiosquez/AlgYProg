507833cac7d4839bc5ab3af9d8525316ac3e3b42
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-04-30 16:39:25 -0300

    Actualización Clase 5

---
+++ Clase_5/Consola/Mascota.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Mascota
+    {
+        // Atributos
+
+        private string nombre;
+
+        private string nombreDelDueño;
+
+        private string especie;
+
+        private string diagnostico;
+
+        private int edad;
+
+        // Constructores
+        public Mascota() { }
+
+        public Mascota(string nombre, string nombreDelDueño, int edad, string especie)
+        {
+            this.nombre = nombre;
+
+            this.nombreDelDueño = nombreDelDueño;
+
+            this.edad = edad;
+
+            this.especie = especie;
+
+            diagnostico = string.Empty;
+        }
+
+        // Propiedades 
+
+        public string Nombre
+        {
+            get { return nombre; }
+
+            set { nombre = value; }
+        }
+
+        public string NombreDelDueño
+        {
+            get { return nombreDelDueño; }
+
+            set { nombreDelDueño = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+
+            set { edad = value; }
+        }
+
+        public string Especie 
+        {
+            get { return especie; }
+
+            set { especie = value; }
+        }
+
+        public string Diagnostico 
+        {
+            get { return diagnostico; }
+
+            set { diagnostico = value; }
+        }
+
+    }
+}
