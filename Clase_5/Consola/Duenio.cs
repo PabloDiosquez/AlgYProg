27ed84e7bb1c6dc02444170f583bdcf271146c51
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-04-30 16:44:59 -0300

    Actualización Clase 5

---
+++ Clase_5/Consola/Duenio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Duenio
+    {
+        // Atributos
+
+        private string nombre;
+
+        private string apellido;
+
+        private Mascota mascota;
+
+        // Constructores 
+
+        public Duenio(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+
+            this.apellido = apellido;   
+        }
+
+        public Duenio(string nombre, string apellido, Mascota mascota) : this(nombre, apellido) 
+        {
+            if (mascota is not null) 
+            {
+                this.mascota = mascota;
+            }
+        }
+
+        // Propiedades 
+
+        public string Nombre 
+        {
+            get { return nombre; }
+
+            set { nombre = value; }
+        }
+    }
+}
