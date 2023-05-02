ae822db8793c441ea873a6b7b6db62e3730cc9cb
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-02 09:25:41 -0300

    Actualización Clase 5

---
+++ Clase_5/Biblioteca/Cola.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Biblioteca
+{
+    //Implementación de la clase Cola (genérica, elementos de clase Object)
+    public class Cola
+    {
+        private ArrayList elementos;
+
+        public Cola()
+        {
+            elementos = new ArrayList();    
+        }
+
+        // Métodos
+
+        public void encolar(object elemento) 
+        {
+            elementos.Add(elemento);
+        }
+
+        public object desencolar()
+        {
+            object elemento = elementos[0];
+
+            elementos.RemoveAt(0);  
+
+            return elemento;
+        }
+
+        public bool estaVacia()
+        {
+            return elementos.Count == 0;
+        }
+
+        public object verTope()
+        {
+            return elementos[0];
+        }
+    }
+}
---
+++ Clase_5/Biblioteca/Pila.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace Biblioteca
+{
+    //Implementación de la clase Pila (genérica, elementos de clase Object)
+    public class Pila
+    {
+        private ArrayList elementos;
+
+        // Constructor 
+
+        public Pila()
+        {
+            elementos = new ArrayList();    
+        }
+
+        // Métodos
+        public void apilar(object elemento)
+        {
+            elementos.Add(elemento);
+        }
+
+        public object desapilar()
+        {
+            object elementoEliminado = elementos[elementos.Count - 1];
+
+            elementos.RemoveAt(elementos.Count - 1);
+
+            return elementoEliminado;
+        }
+
+        public bool estaVacia()
+        {
+            return elementos.Count == 0; 
+        }
+
+        public object verTope()
+        {
+            return elementos[elementos.Count - 1];
+        }
+
+    }
+}
