01a25e23389ad6f83ffb55168409fdd0bef7b155
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-10 15:52:23 -0300

    Actualización Práctica 5

---
+++ Clase_5/Ejercicio_6/ColaDeClientes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Ejercicio_6
+{
+    public class ColaDeClientes
+    {
+        private ArrayList clientes;
+
+        public ColaDeClientes()
+        {
+            clientes = new ArrayList();
+        }
+
+        public void Encolar(Cliente cliente) 
+        {
+            clientes.Add(cliente);
+        }
+
+        public Cliente Desencolar()
+        {
+            Cliente cliente = (Cliente)clientes[0];
+
+            clientes.Remove(cliente);
+
+            return cliente;
+        }
+
+        public bool esVacia()
+        {
+            return clientes.Count == 0;
+        }
+
+        public Cliente verTope()
+        {
+            Cliente primerCliente = (Cliente)clientes[0];
+
+            return primerCliente;
+        }
+    }
+}
