16c91c6e79a2cba01d9c5a2c3fb0a49adacd626d
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-14 15:24:34 -0300

    Actualización Clase 9

--- Clase_9/Clase_9/Busqueda.cs
+++ Clase_9/Clase_9/Busqueda.cs
@@ -26,5 +26,39 @@ namespace Clase_9
 
             return false;
         }
+
+        /// <summary>
+        /// Si la colección en donde buscar se encuentra ordenada entonces es posible utilizar la búsqueda binaria
+        /// que permite encontrar un elemento de forma más rápida que la secuencial.
+        /// Se sitúa la lectura en el centro de la colección y se comprueba si la clave coincide con el valor del
+        /// elementocentral.Si no se encuentra el valor de la clave, se sigue la búsqueda en la mitad inferior o 
+        /// superior del elemento central de la colección.
+        /// </summary>
+        /// <param name="numeros">Array de números enteros</param>
+        /// <param name="clave">Valor destino</param>
+        /// <returns>Booleano</returns>
+        public static bool BusquedaBinaria(int[] numeros, int clave)
+        {
+            int inicio = 0;
+
+            int final = numeros.Length - 1;
+
+            int mitad;
+
+            while (inicio <= final)
+            {
+                mitad = (inicio + final) / 2;
+
+                if (numeros[mitad] == clave)
+                    return true;
+
+                else if (numeros[mitad] < clave) 
+                    inicio = mitad + 1;
+                else
+                    final = mitad - 1;
+            }
+
+            return false;
+        }
     }
 }
