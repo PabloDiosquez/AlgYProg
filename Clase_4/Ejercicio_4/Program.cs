e62a926751446006ec19d232bb6424e8c6982191
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-17 18:42:07 -0300

    Actualización Clase 4

--- Clase_4/Ejercicio_4/Program.cs
+++ Clase_4/Ejercicio_4/Program.cs
@@ -34,5 +34,28 @@ namespace Ejercicios_4_5_6
                 indice++;
             }
         }
+
+        // 5.
+
+        /// <summary>
+        /// Dado un grupo de personas, describe la persona de mayor edad.
+        /// Precondición: El grupo de personas dado no debe estar vacío.
+        /// </summary>
+        /// <param name="personas"></param>
+        /// <returns></returns>
+        public static Persona personaDeMayorEdadDe_(ArrayList personas)
+        {
+            Persona personaDeMayorEdad = (Persona)personas[0];
+
+            foreach (Persona persona in personas)
+            {
+                if (persona.esMayorQue(personaDeMayorEdad))
+                {
+                    personaDeMayorEdad = (Persona)persona;
+                }
+            }
+
+            return personaDeMayorEdad;
+        } 
     }
 }
