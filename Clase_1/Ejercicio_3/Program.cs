24354a64b2507a62a3505c25d0a4662e2a194f5d
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-03 09:41:45 -0300

    Actualización Clase 1 - Ejercicio 3

--- Clase_1/Ejercicio_3/Program.cs
+++ Clase_1/Ejercicio_3/Program.cs
@@ -10,62 +10,92 @@ namespace Ejercicio_3
             //del mes correspondiente.Si el valor ingresado no está en ese rango debe imprimir “Mes inválido ” . Utilice la
             //sentencia switch.
 
-            Console.WriteLine("Ingrese un número de mes:");
-
-            Console.WriteLine(DevolverMes(Console.ReadLine()));
+            Console.WriteLine(mesImpreso());
         }
 
-        public static string DevolverMes(string numeroDeMes)
+        /// <summary>
+        /// Describe un mes del año.
+        /// </summary>
+        /// <returns>String</returns>
+        private static string mesImpreso()
+        {
+            return $"Mes elegido: {mes(validarMes())}";
+        }
+
+        /// <summary>
+        /// Describe un número entre 1 y 12 inclusive, que representa un mes del año.
+        /// </summary>
+        /// <returns>Número entre 1 y 12 inclusive</returns>
+        private static int validarMes()
+        {
+            int nroMes;
+
+            Console.WriteLine("Ingrese un número de mes:");
+
+            while (!int.TryParse(Console.ReadLine(), out nroMes))
+            {
+                Console.WriteLine("Asegúrese de ingresar un número entero entre 1 y 12 inlusive.");
+
+            }
+
+            return nroMes;
+        } 
+
+        /// <summary>
+        /// Describe el mes del año que corresponde al número entero dado.
+        /// Precondición:
+        /// * El número dado debe estar entre 1 y 12 inclusive.
+        /// </summary>
+        /// <param name="numeroDeMes">Mes del año</param>
+        /// <returns>String</returns>
+        private static string mes(int numeroDeMes)
         {
             string mes;
 
             switch (numeroDeMes)
             {
-                case "1":
+                case 1:
                     mes = "Enero";
                     break;
 
-                case "2":
+                case 2:
                     mes = "Febrero";
                     break;
-                case "3":
+                case 3:
                     mes = "Marzo";
                     break;
 
-                case "4":
+                case 4:
                     mes = "Abril";
                     break;
-                case "5":
+                case 5:
                     mes = "Mayo";
                     break;
 
-                case "6":
+                case 6:
                     mes = "Junio";
                     break;
-                case "7":
+                case 7:
                     mes = "Julio";
                     break;
 
-                case "8":
+                case 8:
                     mes = "Agosto";
                     break;
-                case "9":
+                case 9:
                     mes = "Septiembre";
                     break;
 
-                case "10":
+                case 10:
                     mes = "Octubre";
                     break;
-                case "11":
+                case 11:
                     mes = "Noviembre";
                     break;
 
-                case "12":
+                default:
                     mes = "Diciembre";
                     break;
-
-                default:
-                    return "Mes inválido";
             }
 
             return mes;
